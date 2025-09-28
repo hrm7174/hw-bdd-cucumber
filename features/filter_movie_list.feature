4b53dccbb2b6db13d80641bac38b82560f550456
hrm2134 Heidy Mendez <hrm2134@columbia.edu>
2025-09-28 04:26:29 +0000

    Add Cucumber happy paths for sorting and filtering

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -1,36 +1,31 @@
 Feature: display list of movies filtered by MPAA rating
-
   As a concerned parent
   So that I can quickly browse movies appropriate for my family
   I want to see movies matching only certain MPAA ratings
 
-Background: movies have been added to database
+  Background: movies have been added to database
+    Given the following movies exist:
+      | title                   | rating | release_date |
+      | Aladdin                 | G      | 25-Nov-1992  |
+      | The Terminator          | R      | 26-Oct-1984  |
+      | When Harry Met Sally    | R      | 21-Jul-1989  |
+      | The Help                | PG-13  | 10-Aug-2011  |
+      | Chocolat                | PG-13  | 5-Jan-2001   |
+      | Amelie                  | R      | 25-Apr-2001  |
+      | 2001: A Space Odyssey   | G      | 6-Apr-1968   |
+      | The Incredibles         | PG     | 5-Nov-2004   |
+      | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
+      | Chicken Run             | G      | 21-Jun-2000  |
+    And I am on the RottenPotatoes home page
+    Then 10 seed movies should exist
 
-  Given the following movies exist:
-  | title                   | rating | release_date |
-  | Aladdin                 | G      | 25-Nov-1992  |
-  | The Terminator          | R      | 26-Oct-1984  |
-  | When Harry Met Sally    | R      | 21-Jul-1989  |
-  | The Help                | PG-13  | 10-Aug-2011  |
-  | Chocolat                | PG-13  | 5-Jan-2001   |
-  | Amelie                  | R      | 25-Apr-2001  |
-  | 2001: A Space Odyssey   | G      | 6-Apr-1968   |
-  | The Incredibles         | PG     | 5-Nov-2004   |
-  | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
-  | Chicken Run             | G      | 21-Jun-2000  |
+  # First done using only web_steps is fine,
+  # but here’s the DRY version using the custom steps you just added.
+  Scenario: restrict to movies with "PG" or "R" ratings
+    When I check the following ratings: PG, R
+    And I uncheck the following ratings: G, PG-13
+    And I press "ratings_submit"
+    Then I should see only the movies rated: PG, R
 
-  And I am on the RottenPotatoes home page
-  Then 10 seed movies should exist
-
-Scenario: restrict to movies with "PG" or "R" ratings
-  When I check the following ratings: PG, R
-  # enter step(s) to check the "PG" and "R" checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
-  Then complete the rest of of this scenario
-
-Scenario: all ratings selected
-  # your steps here
-  Then complete the rest of of this scenario
+  Scenario: all ratings selected
+    Then I should see all the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,65 +1,62 @@
-# Add a declarative step here for populating the DB with movies.
+require 'date'
 
-Given(/the following movies exist/) do |movies_table|
-  movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+Given(/^(?:the )?following movies exist:?$/) do |movies_table|
+  movies_table.hashes.each do |attrs|
+    if attrs['release_date'].present?
+      begin
+        attrs['release_date'] = Date.parse(attrs['release_date'])
+      rescue ArgumentError
+      end
+    end
+    Movie.create!(attrs)
   end
-  pending "Fill in this step in movie_steps.rb"
 end
 
-Then(/(.*) seed movies should exist/) do |n_seeds|
+Then(/^(.*) seed movies should exist$/) do |n_seeds|
   expect(Movie.count).to eq n_seeds.to_i
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
-
-Then(/^I should see "(.*)" before "(.*)" in the movie list$/) do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  pending "Fill in this step in movie_steps.rb"
+Then(/^I should see "(.*)" before "(.*)"(?: in the movie list)?$/) do |e1, e2|
+  body = page.body
+  expect(body.index(e1)).to be < body.index(e2)
 end
 
-
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I check only the following ratings: PG, G, R"
-
-When(/I check the following ratings: (.*)/) do |rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  pending "Fill in this step in movie_steps.rb"
+When(/^I check the following ratings: (.+)$/) do |rating_list|
+  rating_list.split(/\s*,\s*/).each do |r|
+    steps %Q{ When I check "ratings_#{r}" }
+  end
 end
 
-Then(/^I should (not )?see the following movies: (.*)$/) do |no, movie_list|
-  # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  pending "Fill in this step in movie_steps.rb"
+When(/^I uncheck the following ratings: (.+)$/) do |rating_list|
+  rating_list.split(/\s*,\s*/).each do |r|
+    steps %Q{ When I uncheck "ratings_#{r}" }
+  end
+end
+
+Then(/^I should see the following movies: (.+)$/) do |movie_list|
+  movie_list.split(/\s*,\s*/).each do |title|
+    steps %Q{ Then I should see "#{title}" }
+  end
+end
+
+Then(/^I should not see the following movies: (.+)$/) do |movie_list|
+  movie_list.split(/\s*,\s*/).each do |title|
+    steps %Q{ Then I should not see "#{title}" }
+  end
+end
+
+Then(/^I should see only the movies rated: (.+)$/) do |ratings|
+  wanted = ratings.split(/\s*,\s*/)
+  Movie.where(rating: wanted).pluck(:title).each do |t|
+    steps %Q{ Then I should see "#{t}" }
+  end
+  Movie.where.not(rating: wanted).pluck(:title).each do |t|
+    steps %Q{ Then I should not see "#{t}" }
+  end
 end
 
 Then(/^I should see all the movies$/) do
-  # Make sure that all the movies in the app are visible in the table
-  pending "Fill in this step in movie_steps.rb"
-end
-
-### Utility Steps Just for this assignment.
-
-Then(/^debug$/) do
-  # Use this to write "Then debug" in your scenario to open a console.
-  require "byebug"
-  byebug
-  1 # intentionally force debugger context in this method
-end
-
-Then(/^debug javascript$/) do
-  # Use this to write "Then debug" in your scenario to open a JS console
-  page.driver.debugger
-  1
-end
-
-Then(/complete the rest of of this scenario/) do
-  # This shows you what a basic cucumber scenario looks like.
-  # You should leave this block inside movie_steps, but replace
-  # the line in your scenarios with the appropriate steps.
-  raise "Remove this step from your .feature files"
+  Movie.pluck(:title).each do |t|
+    expect(page).to have_content(t)
+  end
 end
